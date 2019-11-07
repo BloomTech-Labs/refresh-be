eae5bad67e233642052ce039b272903d9548304d
roman-jordan <rjordan1352@gmail.com>
2019-11-06 22:44:52 -0800

    google auth

--- api/auth/preAuth/google.js
+++ api/auth/preAuth/google.js
@@ -1,25 +1,25 @@
-const gitHubRouter = require("express").Router();
+const googleRouter = require("express").Router();
 const passport = require("passport");
-const GitHubStrategy = require("passport-github").Strategy;
+const GitHubStrategy = require("passport-google-oauth").Strategy;
 const jwt = require(_jwt)
 //Config GitHub Auth
-const gitId = process.env.GITHUB_CLIENT_ID;
-const gitSecret = process.env.GITHUB_CLIENT_SECRET;
-const gitRedirect = "https://refresh-yo.herokuapp.com/gitAuth/return";
+const googleId = process.env.GOOGLE_CLIENT_ID;
+const googleSecret = process.env.GOOGLE_CLIENT_SECRET;
+const googleRedirect = "https://refresh-yo.herokuapp.com/googleAuth/return";
 
 //Bring in the userModel
 const User = require("../authModel");
 
 //InitialIze PassPort
-gitHubRouter.use(passport.initialize());
+googleRouter.use(passport.initialize());
 
 //Declare Strategy Vars
 passport.use(
-  new GitHubStrategy(
+  new GoogleStrategy(
     {
-      clientID: gitId,
-      clientSecret: gitSecret,
-      callbackURL: gitRedirect,
+      clientID: googleId,
+      clientSecret: googleSecret,
+      callbackURL: googleRedirect,
       session: false
     },
     function(accessToken, refreshToken, profile, done) {
@@ -30,12 +30,12 @@ passport.use(
 );
 
 //GitLogin URL
-gitHubRouter.get("/", passport.authenticate("github"));
+googleRouter.get("/", passport.authenticate("google"));
 
 //Github Call Back
-gitHubRouter.get(
+googleRouter.get(
   "/return",
-  passport.authenticate("github", {
+  passport.authenticate("google", {
     failureRedirect: "/login",
     session: false,
     scope: ['email']
@@ -56,4 +56,8 @@ gitHubRouter.get(
     res.send(Buffer.from(setToken))
   });
 
-module.exports = gitHubRouter;
+  googleRouter.get("/terms", (req,res) =>{
+    res.status(200).json({message:'Pretty much, we use your email to create your account, if you want to disconnect from your registration median, select forgot password and you will be switched to our local auth stratagey. If you want to nuke your account, go to settings and click the nuke button. We will do our best to protect your data, however, where there is a will there is a way. That being said, we take zeor liablity for any data breaches. Your data may be used for internal and external purposes, but, mainly to improve our product for you.'});
+  });
+
+module.exports = googleRouter;
