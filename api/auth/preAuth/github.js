3213de0c0cc4f45a43818524911c9ffb7db2cc18
roman-jordan <rjordan1352@gmail.com>
2019-11-07 12:35:05 -0800

    Really dis like heroku

--- api/auth/preAuth/github.js
+++ api/auth/preAuth/github.js
@@ -5,7 +5,7 @@ const GitHubStrategy = require("passport-github").Strategy;
 //Config GitHub Auth
 const gitId = process.env.GITHUB_CLIENT_ID;
 const gitSecret = process.env.GITHUB_CLIENT_SECRET;
-const gitRedirect = "https://refresh-yo.herokuapp.com/gitAuth/return";
+const gitRedirect = "https://apidevnow.com/gitAuth/return";
 
 //Bring in the userModel
 const User = require("../authModel");
