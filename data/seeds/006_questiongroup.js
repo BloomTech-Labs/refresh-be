b9c2ce46234898b9da14bb3038e1bc9817ba95e1
roman-jordan <rjordan1352@gmail.com>
2019-11-29 06:12:38 -0800

    Testing Work Flow

--- data/seeds/006_questiongroup.js
+++ data/seeds/006_questiongroup.js
@@ -10,6 +10,8 @@ exports.seed = function(knex) {
       ? '[1,2,3,4,5,6,7,8,9,10]'
       : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+      console.log(process.env.NODE_ENV)
+
       return knex('question_groups').insert([
         {group: 'onboarding', question_ids: questions}
        
