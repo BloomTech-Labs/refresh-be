5c0b9243b34a0de905c82594db23625efd94035a
roman-jordan <rjordan1352@gmail.com>
2019-11-30 15:22:04 -0800

    Intigrated for windows, mac and linux testing

--- data/seeds/006_questiongroup.js
+++ data/seeds/006_questiongroup.js
@@ -1,12 +1,11 @@
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
   return knex("question_groups")
     .del()
     .then(function() {
       // Sqlite 3 does not support Int[]
-      const questions =
-        
-           [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      const questions = process.env.NODE_ENV === "test"
+        ? "[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
+        : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
           
 
       return knex("question_groups").insert([
