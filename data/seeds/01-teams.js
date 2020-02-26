3be17c60564e6fc25a3269306680fb10e8bb85a1
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-02-25 23:18:43 -0600

    added getAllTeams, getTeamById, and addTeam routes; took out IDs from teams seed file

--- data/seeds/01-teams.js
+++ data/seeds/01-teams.js
@@ -2,8 +2,8 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
       return knex('teams').insert([
-        {id: 1, name: 'Accounting'},
-        {id: 2, name: 'Developers'},
-        {id: 3, name: 'Human Resource'}
+        {name: 'Accounting'},
+        {name: 'Developers'},
+        {name: 'Human Resource'}
     ]);
 };
--- teams/teams-router.js
+++ teams/teams-router.js
@@ -4,7 +4,48 @@ const Teams = require('./teams-model.js');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const AllTeams = await Teams.getAllTeams()
+        res.status(200).json(AllTeams)
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({ error: 'Could not retrieve teams from the database' })
+    }
+})
 
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    Teams.getTeamById(id)
+        .then(team => {
+            if (team) {
+                res.json(team);
+            } else {
+                res.status(404).json({
+                    message: "Could not find team with given id.",
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Failed to get team" });
+        });
+});
+
+router.post('/', async (req, res) => {
+    const team = req.body;
+    try {
+        if(team) {
+            const AddedTeam = await Teams.addTeam(team)
+            res.status(200).json(AddedTeam)
+        } else {
+            res.status(400).json({ error: 'Please provide a team' })
+        }
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({ error: 'Could not add team to the database' })
+    }
+})
 
 
 
