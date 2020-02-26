de2befc69e53b657ab2bf61ddbd4ed8a8d9e03f5
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-02-25 23:03:13 -0600

    added getAllTeams, getTeamById, and addTeam to teams-model

--- teams/teams-model.js
+++ teams/teams-model.js
@@ -1,1 +1,24 @@
 const db = require('../data/db-config');
+
+module.exports = {
+    getAllTeams,
+    getTeamById,
+    addTeam
+}
+
+function getAllTeams() {
+    return db('teams').select('id','name')
+}
+
+function getTeamById(id) {
+    return db('teams').where({ id }).first();
+}
+
+function addTeam(team) {
+    return db('teams')
+        .insert(team, 'id')
+        .then(ids => {
+            const [id] = ids;
+            return getTeamById(id)
+        })
+}
