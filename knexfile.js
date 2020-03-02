8d89c4f8e3b9cb9b2110a7c716c4419403068666
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-03-02 14:58:11 -0600

    set up for heroku

--- knexfile.js
+++ knexfile.js
@@ -31,18 +31,17 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
 
