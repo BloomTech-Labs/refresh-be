6a391267839c27199fa0b6b8a4abda6bf52f81ed
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-03-02 15:56:50 -0600

    changed environment default to production

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
 
-const environment = process.env.DB_ENV || 'development';
+const environment = process.env.DB_ENV || 'production';
 
 module.exports = knex(config[environment]);
