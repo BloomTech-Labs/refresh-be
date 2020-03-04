4dafd33597ec782c98b02c7a9bb496f9db55052f
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-03-04 11:17:51 -0600

    added getAdminProfileByid and addAdmin to model

--- admin/admin-router.js
+++ admin/admin-router.js
@@ -3,6 +3,27 @@ const Admin = require('../admin/admin-model');
 const jwt = require('jsonwebtoken')
 const { jwtSecret } = require('../config/secrets.js')
 
+
+
+router.post('/register', async (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 8);
+    user.password = hash;
+
+    try {
+        if(user) {
+            const AddUser = await Users.addUser(user)
+            const token = signToken(user)
+            res.status(201).json({ message: `Thank you for registering, ${user.first_name}!`, add: AddUser, token: token })
+        } else {
+            res.status(400).json({ errorMessage: 'Please fill out all required fields' })
+        }
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({ errorMessage: 'Error adding user to the database' })
+    }
+})
+
 router.post('/login', (req, res) => {
     let { email, password } = req.body;
     Admin.getAdminBy({ email })
