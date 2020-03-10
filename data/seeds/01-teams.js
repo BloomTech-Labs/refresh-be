c8a4a00eb15b9e4b6c2074952e2698b0cd7e823c
Justin Brandley <brandleyj@gmail.com>
2020-03-10 11:20:41 -0500

    added seed data

--- data/seeds/01-teams.js
+++ data/seeds/01-teams.js
@@ -1,9 +1,12 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-      return knex('teams').insert([
-        {name: 'Accounting', points: '0'},
-        {name: 'Developers', points: '0'},
-        {name: 'Human Resource', points: '0'}
-    ]);
+	// Deletes ALL existing entries
+	return knex("teams").insert([
+		{ name: "Accounting", points: "0" },
+		{ name: "Developers", points: "0" },
+		{ name: "Human Resource", points: "0" },
+		{ name: "Sales 1", points: "0" },
+		{ name: "Sales 2", points: "0" },
+		{ name: "Marketing", points: "0" },
+		{ name: "Warehouse", points: "0" }
+	]);
 };
