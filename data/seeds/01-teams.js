aaf20eefd0414ffd234e98fdc7d51aee96b486d9
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-03-16 13:51:44 -0500

    added points to seed data

--- data/seeds/01-teams.js
+++ data/seeds/01-teams.js
@@ -1,12 +1,12 @@
 exports.seed = function(knex) {
 	// Deletes ALL existing entries
 	return knex("teams").insert([
-		{ name: "Accounting", points: "0" },
-		{ name: "Developers", points: "0" },
-		{ name: "Human Resource", points: "0" },
-		{ name: "Sales 1", points: "0" },
-		{ name: "Sales 2", points: "0" },
-		{ name: "Marketing", points: "0" },
-		{ name: "Warehouse", points: "0" }
+		{ name: "Accounting", points: "150" },
+		{ name: "Developers", points: "100" },
+		{ name: "Human Resource", points: "75" },
+		{ name: "Sales 1", points: "60" },
+		{ name: "Sales 2", points: "55" },
+		{ name: "Marketing", points: "10" },
+		{ name: "Warehouse", points: "5" }
 	]);
 };
