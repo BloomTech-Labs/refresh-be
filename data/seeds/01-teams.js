baf2b0f1ecc2d7c0311432a1975dcb43da5aa8ae
shawn <shawnpatel196@gmail.com>
2020-05-14 11:00:04 -0500

    deleted team points seed data

--- data/seeds/01-teams.js
+++ data/seeds/01-teams.js
@@ -1,12 +1,12 @@
 exports.seed = function(knex) {
 	// Deletes ALL existing entries
 	return knex("teams").insert([
-		{ name: "Accounting", points: "150" },
-		{ name: "Developers", points: "100" },
-		{ name: "Human Resource", points: "75" },
-		{ name: "Sales 1", points: "60" },
-		{ name: "Sales 2", points: "55" },
-		{ name: "Marketing", points: "10" },
-		{ name: "Warehouse", points: "5" }
+		{ name: "Accounting"},
+		{ name: "Developers"},
+		{ name: "Human Resource"},
+		{ name: "Sales 1"},
+		{ name: "Sales 2"},
+		{ name: "Marketing"},
+		{ name: "Warehouse"}
 	]);
 };
